3839ff5ad14e9d95796c7695b0e9a4df2011e5ba
Anas Hussam <anashuasam4@gmail.com>
2025-07-22 11:45:35 +0300

    Three programming problems related to integers have been solved. This section will continue to be developed until the completion of the 10-problem series.

---
+++ Perfect Number From 1 to N/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+class program
+{
+   
+    static int ReadPositiveNumber(string message)
+    {
+
+        int number;
+        do
+        {
+            Console.WriteLine(message);
+        } while (!int.TryParse(Console.ReadLine(), out number) || number <= 0);
+
+        return number;
+    }
+
+    static bool IsPerfectNumber(int number)
+    {
+
+        int sum = 0;
+
+        for (int i = 1; i < number; i++)
+        {
+            if (number % i == 0)
+                sum += i;
+        }
+
+        return number == sum;
+    }
+
+
+
+
+
+    static void PrintNumberPerfectFROM_1_to_N(int number)
+    {
+        for (int i = 1; i <= number; i++)
+            if (IsPerfectNumber(i))
+                Console.WriteLine(i);
+
+    }
+
+    static void Main()
+    {
+
+        PrintNumberPerfectFROM_1_to_N(ReadPositiveNumber("Please enter a positive number:"));
+
+
+    }
+
+}
---
+++ Print Digits in a Reversed Order/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+class Program
+{
+    static int ReadPositiveNumber(string message)
+    {
+
+        int number;
+        do
+        {
+            Console.WriteLine(message);
+        } while (!int.TryParse(Console.ReadLine(), out number) || number <= 0);
+
+        return number;
+    }
+
+    static void PrintDigits(int num)
+    {
+        int reverse = 0;
+        while(num >0)
+        {
+            reverse = num % 10; // تخزين باقي القسمة من الرقم 
+            num = num / 10;//ازالة للرقم الاخير 
+            Console.WriteLine($"Reversed number is: {reverse}");
+        }
+    }
+
+    static void PrintDigits1(int num)
+    {
+        int reverse = 0, digits = 0;
+        while (num > 0)
+        {
+             digits = num % 10;
+            reverse = reverse * 10 + digits;
+            num = num / 10; 
+            
+        }
+        Console.WriteLine($"Reversed number is: {reverse}");
+    }
+
+   static  int SumDigits(int num)
+    {
+        int sum = 0, reverse = 0;
+        while (num > 0)
+        {
+            reverse = num % 10;
+            num = num / 10;
+            sum = sum + reverse;
+        }
+        return sum;
+    }
+    static void Main()
+    {
+        //PrintDigits(ReadPositiveNumber("Please enter a positive number:"));
+        //PrintDigits1(ReadPositiveNumber("Please enter a positive number:"));
+        
+      Console.WriteLine($"Sum Of Digits = {SumDigits(ReadPositiveNumber("Please enter a positive number:"))}");
+
+
+
+    }
+}
